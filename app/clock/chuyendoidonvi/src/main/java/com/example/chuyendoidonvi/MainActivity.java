ba52d2f9064487e9ddca690bbb73c1d248a824a0
admin <sonph2013@gmail.com>
2025-01-11 14:41:05 +0700

    send to me

--- app/clock/chuyendoidonvi/src/main/java/com/example/chuyendoidonvi/MainActivity.java
+++ app/clock/chuyendoidonvi/src/main/java/com/example/chuyendoidonvi/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.chuyendoidonvi;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Log.d("KTQD", "Phuong thuc onCreate() duoc goi");
+
+        TextView textView1 = findViewById(R.id.textView1);
+        textView1.setText("Lap trinh android");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("KTQD", "Phuong thuc onStart() duoc goi");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("KTQD", "Phuong thuc onResume() duoc goi");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("KTQD", "Phuong thuc onPause() duoc goi");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("KTQD", "Phuong thuc onStop() duoc goi");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("KTQD", "Phuong thuc onDestroy() duoc goi");
+
     }
 }
